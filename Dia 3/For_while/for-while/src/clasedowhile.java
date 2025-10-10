c77c173310fd4a06742a42b6c645c12c5eea3c67
juansotelo1709 <juan.david.jds84@gmail.com>
2025-10-10 00:17:32 -0500

    tercer dia

---
+++ Dia 3/For_while/for-while/src/clasedowhile.java
@@ -0,0 +1,14 @@
+public class clasedowhile {
+
+    public static void main(String[] args) {
+
+        System.out.println(
+                "Clase do-while: es una estructura de control que permite repetir un bloque de código al menos una vez, y luego continuar repitiéndolo mientras una condición sea verdadera.");
+        int i = 0;
+        do {
+            System.out.println("Valor de i: " + i); // imprime el valor de i
+            i++; // incrementa el valor de i en 1
+        } while (i < 5);
+    }
+
+}
---
+++ Dia 3/For_while/for-while/src/clasewhile.java
@@ -0,0 +1,14 @@
+public class clasewhile {
+
+    public static void main(String[] args) {
+
+        System.out.println(
+                "Clase while: es una estructura de control que permite repetir un bloque de código mientras una condición sea verdadera.");
+        int i = 0;
+        while (i < 5) {
+            System.out.println("Valor de i: " + i); // imprime el valor de i
+            i++; // incrementa el valor de i en 1
+        }
+    }
+
+}
