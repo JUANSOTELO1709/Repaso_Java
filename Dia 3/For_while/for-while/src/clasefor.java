eeeadbdd6f839289b9ba65a191ebe373328cec0f
juansotelo1709 <juan.david.jds84@gmail.com>
2025-10-10 00:10:58 -0500

    tercer dia

--- Dia 3/For_while/for-while/src/clasefor.java
+++ Dia 3/For_while/for-while/src/clasefor.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class clasefor {
+
+    public static void main(String[] args) {
+
+        System.out.println(
+                "Clase for: es una estructura de control que permite repetir un bloque de código un número determinado de veces.");
+        for (int i = -10; i < 5; i++) {
+            System.out.println("Valor de i: " + i);
+
+        }
+    }
+
 }
